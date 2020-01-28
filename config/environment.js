e05a47aafb1d77303afa3cf240437175dea69bd1
Julie Hutchinson <julie.gomez001@mymdc.net>
2020-01-28 14:55:28 -0800

    [fix] import env keys only while in development for deployment purposes

--- config/environment.js
+++ config/environment.js
@@ -1,10 +1,10 @@
 // Setup environment
 const currentEnvironment = process.env.NODE_ENV || 'development';
-const secretKeys = require('./keys');
 
 // Sets env variables when not in production
 module.exports = () => {
   if (currentEnvironment === 'development') {
+    const secretKeys = require('./keys');
     const environmentVars = secretKeys[currentEnvironment];
 
     Object.keys(environmentVars).forEach(envVar => {
